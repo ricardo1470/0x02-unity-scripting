bfd10b5838cc51ce24fef1850a6c7eef25c9e614
Ricardo Alfonso Camayo Erazo <ricardo.alfonso.camayo@gmail.com>
2020-12-23 01:26:14 -0500

    add script move sphere

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = gameObject.GetComponent<Rigidbody> ();
+    }
+
+    // functions to allow the Player to move when either
+    // the WASD or arrow keys are pressed
+    void FixedUpdate()
+    {
+        float Horizontal = Input.GetAxis("Horizontal");
+        float Vertical = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(Horizontal, 0.0f, Vertical);
+
+        rigid.AddForce (move * speed);
+    }
+}
