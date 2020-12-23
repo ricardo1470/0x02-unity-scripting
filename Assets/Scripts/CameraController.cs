1df989c696ae59be0bb0f56b09ef1d9bbf54f9d9
Ricardo Alfonso Camayo Erazo <ricardo.alfonso.camayo@gmail.com>
2020-12-23 02:41:27 -0500

    add script move camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform player;
+	public Vector3 cameraOffset;
+
+	void Start ()
+    {
+        cameraOffset = transform.position - player.transform.position;
+    }
+	void LateUpdate ()
+    {
+		Vector3 newPosition = player.transform.position + cameraOffset;
+        player.position = newPosition;
+	}
+}
